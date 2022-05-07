bda001b000f77802285a0e2f9e22ad97d08deae2
Jagadeesh C <jagadeesh@shipx.in>
2022-05-07 14:26:03 +0530

    update sqlalchemy database uri

--- market/__init__.py
+++ market/__init__.py
@@ -7,15 +7,15 @@ import re
 
 app = Flask(__name__)
 
-# uri = os.getenv("DATABASE_URL")
-# if uri.startswith("postgres://"):
-#     uri = uri.replace("postgres://", "postgresql://", 1)
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ocbmivptovdxou:09bc7f2c554c8eedd46fd3dc154f3a875b17fb4ad29b8a17b01711b10c8376e5@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d57v8hpoh477kg'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
